006019a37eeff2ed14f2c05945a970d0887ce10e
Ben Balderas <benbalderas3@gmail.com>
2021-10-01 16:47:13 -0500

    Fix query

--- components/more-stories.js
+++ components/more-stories.js
@@ -1,22 +1,22 @@
-import PostPreview from '../components/post-preview'
+import PostPreview from '../components/post-preview';
 
 export default function MoreStories({ posts }) {
-  return (
-    <section>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-8 lg:gap-12 mb-32">
-        {posts.map(({ node }) => (
-          <PostPreview
-            key={node._meta.uid}
-            title={node.title}
-            tag={node.tag}
-            coverImage={node.coverimage}
-            date={node.date}
-            author={node.author}
-            slug={node._meta.uid}
-            excerpt={node.excerpt}
-          />
-        ))}
-      </div>
-    </section>
-  )
+   return (
+      <section>
+         <div className='grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-8 lg:gap-12 mb-32'>
+            {posts.map(({ node }) => (
+               <PostPreview
+                  key={node._meta.uid}
+                  title={node.title}
+                  category={node.category}
+                  coverImage={node.coverimage}
+                  date={node.date}
+                  author={node.author}
+                  slug={node._meta.uid}
+                  excerpt={node.excerpt}
+               />
+            ))}
+         </div>
+      </section>
+   );
 }
--- components/post-preview.js
+++ components/post-preview.js
@@ -1,45 +1,49 @@
-import Link from 'next/link'
-import { RichText } from 'prismic-reactjs'
-import Avatar from '../components/avatar'
-import Date from '../components/date'
-import CoverImage from './cover-image'
+import Link from 'next/link';
+import { RichText } from 'prismic-reactjs';
+import Avatar from '../components/avatar';
+import Date from '../components/date';
+import CoverImage from './cover-image';
 
 export default function PostPreview({
-  title,
-  tag,
-  coverImage,
-  date,
-  excerpt,
-  author,
-  slug,
+   title,
+   category,
+   coverImage,
+   date,
+   excerpt,
+   author,
+   slug
 }) {
-  return (
-    <div>
-      <div className="mb-5">
-        <CoverImage
-          title={RichText.asText(title)}
-          slug={slug}
-          url={coverImage.url}
-        />
+   return (
+      <div>
+         <div className='mb-5'>
+            <CoverImage
+               title={RichText.asText(title)}
+               slug={slug}
+               url={coverImage.url}
+            />
+         </div>
+
+         <h3 className='text-3xl mb-3 leading-snug'>
+            <Link href={`/posts/${slug}`}>
+               <a className='hover:underline'>
+                  <RichText render={title} />
+               </a>
+            </Link>
+         </h3>
+
+         <h4 className='mb-4'>
+            <Link href={`/categories/${category._meta.uid}`}>
+               <a className='hover:underline'>{category.name[0].text}</a>
+            </Link>
+         </h4>
+
+         <p className='text-lg leading-relaxed mb-4'>{excerpt}</p>
+
+         <div className='text-lg mb-4'>
+            <Date dateString={date} />
+         </div>
+
+         <Avatar name={author.name} picture={author.picture} />
       </div>
-
-      <h3 className="text-3xl mb-3 leading-snug">
-        <Link href={`/posts/${slug}`}>
-          <a className="hover:underline">
-            <RichText render={title} />
-          </a>
-        </Link>
-      </h3>
-
-      <h4 className="mb-4">{tag}</h4>
-
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-
-      <div className="text-lg mb-4">
-        <Date dateString={date} />
-      </div>
-
-      <Avatar name={author.name} picture={author.picture} />
-    </div>
-  )
+   );
 }
